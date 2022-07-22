20fc50582b29979bab3205dc14079328a47063ea
Simon MOLLET <s.mollet@attineos.com>
2022-07-22 13:04:55 +0200

    improve responsive

--- src/Games/flag/components/game/Question.jsx
+++ src/Games/flag/components/game/Question.jsx
@@ -34,7 +34,10 @@ export function Question({
 
 function Round(props) {
   return (
-    <div className="hidden xl:flex justify-center items-center border-2 shadow-lg border-primary shadow-primary -skew-x-6   h-20 w-28 text-primary font-bold">
+    <div
+      className="hidden xl:flex justify-center items-center border-2 shadow-md border-primary 
+    shadow-primary -skew-x-6 rounded-md h-20 w-28 text-primary font-bold"
+    >
       <h2 className="font-bold text-primary lg:text-2xl">
         {props.round + 1}/{props.numberRound}
       </h2>
